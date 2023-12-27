05d83b02e22e535beba6ce30dde571d67dcba551
David Allen <spondicious@me.com>
2023-12-27 21:51:21 +0100

    not going well

--- a_plot/models.py
+++ a_plot/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 from django.templatetags.static import static
 
@@ -75,9 +76,6 @@ SEASONS = (
 )
 
 
-
-
-
 class Country(models.Model):
     name = models.CharField(max_length=100)
 
@@ -106,6 +104,7 @@ class Tag(models.Model):
 class Plot(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    likes = models.ManyToManyField("auth.User", related_name="likedplots", through="LikedPlot")
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     season = models.CharField(max_length=100, choices=SEASONS, default="Mid", blank=True, null=True)
     image = models.URLField(blank=True, null=True)
@@ -119,10 +118,27 @@ class Plot(models.Model):
     id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return self.title
+        return f'{self.user.username} : {self.plot.title}'
     
     class Meta:
         ordering = ["-list_date"]
+        
+        
+        
+class LikedPlot(models.Model):
+    plot = models.ForeignKey(Plot, on_delete=models.CASCADE)
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.user.username} : {self.plot.title}'
+    
+    class Meta:
+        ordering = ["-created"]
+        unique_together = ("plot", "user")
+    
+ 
+
 
 
 
